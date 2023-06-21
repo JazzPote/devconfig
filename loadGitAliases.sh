b6808c6875f472b66461dd68f69c386cdfde2a60
Guilhem Forey <guilhem.forey@hotmail.fr>
2023-06-21 19:52:29 +0100

    Create git alias: pnew

--- loadGitAliases.sh
+++ loadGitAliases.sh
@@ -13,6 +13,7 @@ createAlias delsave "br -D \"save--$(git symbolic-ref --short HEAD)\""
 createAlias l log
 createAlias pf "push --force"
 createAlias pfl "push --force-with-lease"
+createAlias pnew "!git push --set-upstream origin \$(git symbolic-ref --short HEAD)"
 createAlias pur "pull --rebase"
 createAlias rb rebase
 createAlias rbi "rebase -i"
